37f7a55253e4bdc2285f3f79993a44a508a1edd4
DenGolivets <oxytechmusic@gmail.com>
2024-02-27 19:46:09 +0200

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import Header from "./Components/Header";
+
 export default function Home() {
   return (
     <>
       <main>
-        <header>head head</header>
+        <header>
+          <Header />
+        </header>
         <section id="about us">Hello World!</section>
         <section id="services"></section>
         <section id="testymanials"></section>
